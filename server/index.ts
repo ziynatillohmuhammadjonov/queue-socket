555939b182693aabba5d2c571c614c1d2335322f
Ziynatillo <zmorzugrand@gmail.com>
2024-04-06 15:00:31 +0500

    add some func

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import { createServer } from 'node:http'
 import { Server, Socket } from 'socket.io'
 import dotenv from "dotenv";
 import cors from 'cors';
+import { channel } from "node:process";
 
 dotenv.config();
 
@@ -18,7 +19,7 @@ const io = new Server(server, {
     methods: ["GET", "POST"]
   }
 })
-
+let interval: any
 app.get('/rooms', (req: Request, res: Response) => {
   res.send(rooms)
 })
@@ -29,7 +30,7 @@ app.get("/", (req: Request, res: Response) => {
 io.on('connection', (socket: Socket) => {
   socket.on('join-room', (data: string) => {
     socket.join(data);
-    console.log(`${socket.id} joined room ${data}`);
+    // console.log(`${socket.id} joined room ${data}`);
   });
 
   // socket.on('test-msg', (data: { place: string, customerNumber: any }) => {
@@ -39,24 +40,63 @@ io.on('connection', (socket: Socket) => {
   //     console.log(`Socket is not in room ${data.place}`);
   //   }
   // });
-  socket.on('join-driver', (data:{place:string, carNumber:string})=>{
+  console.log(io.sockets.adapter.rooms)
+  socket.on('join-driver', (data: { place: string, carNumber: string }) => {
     socket.join(data.place)
-    console.log(data.carNumber+' joined '+data.place)
-    io.in(data.place).emit('add-driver', data)
+    // console.log(data.carNumber+' joined '+data.place)
+
+
+
   })
   // kelgan buyurtmani tegishli xonaga yuboramiz
-  socket.on('add-order',(data:{place:string, customerNumber:string})=>{
+  socket.on('add-order', (data: { place: string, customerNumber: string }) => {
     io.in(data.place).emit("post-order", data)
+
   })
 
   // Обработка события 'leave-room'
   socket.on('leave-room', (room: string) => {
     socket.leave(room);
-    console.log(`${socket.id} left room ${room}`);
+    // console.log(`${socket.id} left room ${room}`);
+
   });
 
   // Дополнительный код
+  // Функция для отправки сообщений поочередно
+  function startSendingMessages(channel: string) {
+    const room = io.sockets.adapter.rooms.get(channel);
+    const users: string[] = [];
+    if (room) {
+      room.forEach(socketId => {
+        const socket = io.sockets.sockets.get(socketId);
+        if (socket) {
+          users.push(socket.id); // or any other socket property you need
+        }
+      });
+      console.log(users);
+    } else {
+      // Handle the case where the room doesn't exist
+    }
+    let index = 0;
+
+    interval = setInterval(() => {
+      if (messageQueues[channel].length > 0) {
+        const message = messageQueues[channel].shift();
+        const userId = users[index];
+
+        // Отправляем сообщение только определенному пользователю
+        io.to(userId).emit('newMessage', message);
+
+        // Переходим к следующему пользователю
+        index = (index + 1) % users.length;
+      } else {
+        clearInterval(interval);
+      }
+    }, 5000); // Интервал между отправкой сообщений (в миллисекундах)
+  }
+
 });
+
 
 
 
